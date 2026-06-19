68155a44ca35fb16b913299a06e35374c0d71d73
marcantonioo <dinizsilvam3@gmail.com>
2026-06-19 11:46:31 -0300

    refatorando schemas. Closes #45

---
+++ backend/app/schemas/schemas.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, EmailStr
+
+class LoginSchema(BaseModel):
+    email: EmailStr
+    senha: str
+
+class SetorSchema(BaseModel):
+    nome: str
+    sigla: str
+
+class UsuarioCadastroSchema(BaseModel):
+    nome: str
+    email: EmailStr
+    senha: str
+    
+class TokenSchema(BaseModel):
+    access_token: str
+    token_type: str
+    usuario_id: int
+    perfil: str
+
+class PedidoSchema(BaseModel):
+    titulo: str
+    descricao: str
+    setor_solicitante_id: int
+    setor_responsavel_id: int
+    prioridade: str
+ 
